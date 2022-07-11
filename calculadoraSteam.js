edd14eb9aca9def2be9248133f6d72ec80afd331
Leo-Cata <leonel.m.cata@gmail.com>
2022-07-11 17:04:09 -0300

    first commit

---
+++ calculadoraSteam.js
@@ -0,0 +1,22 @@
+function calculate() {
+    let calculate = prompt("Calcular Precio Con Impuesto? si/no");
+    let gameIndex = ["Forza Horizon 5", "Raft", "Elden Ring", "Project Zomboid"]
+    while (calculate == "si") {
+        let gameName = prompt("Ingrese nombre del juego");
+        if (!gameIndex.includes(gameName)) {
+            gameIndex.push(gameName);
+        }
+        console.log(gameIndex);
+        let gamePrice = parseFloat(prompt("Ingrese El Precio En Pesos"));
+        let gameTax = parseFloat(prompt("Ingrese El Valor De Los Impuestos En Numeros. (Generalmente 65%)"));
+        let finalPrice = gamePrice + (gamePrice * gameTax / 100);
+        console.log("Precio Final A Pagar Por", gameName, "Es: ", finalPrice);
+        let recalculate = prompt("Calcular Precio Con Impuesto? si/no");
+        if (recalculate == "no") {
+            console.log("Que Tenga Un Buen Dia");
+            break
+        }
+    }
+}
+
+calculate()
