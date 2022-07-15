3db44cdaf7df85fc4b9a2aeda0185e85cc86a3cf
Leo-Cata <leonel.m.cata@gmail.com>
2022-07-14 23:28:11 -0300

    DOM interactuando con HTML clase 8

--- calculadoraSteam.js
+++ calculadoraSteam.js
@@ -1,3 +1,5 @@
+let resultado = document.getElementById("resultado");
+
 function calculate() {
     let calculate = prompt("Calcular Precio Con Impuesto? si/no");
     let gameIndex = ["Forza Horizon 5", "Raft", "Elden Ring", "Project Zomboid"]
@@ -10,7 +12,10 @@ function calculate() {
         let gamePrice = parseFloat(prompt("Ingrese El Precio En Pesos"));
         let gameTax = parseFloat(prompt("Ingrese El Valor De Los Impuestos En Numeros. (Generalmente 65%)"));
         let finalPrice = gamePrice + (gamePrice * gameTax / 100);
-        console.log("Precio Final A Pagar Por", gameName, "Es: ", finalPrice);
+
+        // console.log("Precio Final A Pagar Por", gameName, "Es: ", finalPrice);
+        resultado.innerHTML = `<p>Precio Final A Pagar Por ${gameName} Es: ${finalPrice}`; //console log trasladado a que crea un p por fuera del form
+
         let recalculate = prompt("Calcular Precio Con Impuesto? si/no");
         if (recalculate == "no") {
             console.log("Que Tenga Un Buen Dia");
@@ -18,5 +23,8 @@ function calculate() {
         }
     }
 }
+
+
+
 
 calculate()
