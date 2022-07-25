31c3166dee1c86d1d41d756698ba18e7268a5b9b
Leo-Cata <leonel.m.cata@gmail.com>
2022-07-25 12:55:39 -0300

    localStorage for tax and prov imp value

--- calculadoraSteam.js
+++ calculadoraSteam.js
@@ -1,36 +1,53 @@
 let prov = document.getElementById("prov");
 let provImp;
-prov.addEventListener("change", function () {// get prov option value
+prov.addEventListener("change", function () { // get prov option value
     provImp = parseFloat(prov.value)
 })
 
 let gameValue = document.getElementById("gameValue");
 let precio;
-gameValue.addEventListener("change", function () {// get value input 
+gameValue.addEventListener("change", function () { // get value input 
     precio = parseFloat(gameValue.value)
 })
 
 let gameTax = document.getElementById("taxValue");
-let tax;
-gameTax.addEventListener("change", function () {// get tax value
-    tax = parseInt(gameTax.value)
+let taxValue;
+gameTax.addEventListener("change", function () { // get tax value
+    taxValue = parseFloat(gameTax.value)
 })
 
 let gameName = document.getElementById("gameName")
 let nombreGame;
-gameName.addEventListener("change",function(){// get name 
+gameName.addEventListener("change", function () { // get name 
     nombreGame = gameName.value;
 })
 
 let calcularSubmit = document.getElementById("btn__calcular");
 let resultado = document.getElementById("resultado");
-calcularSubmit.addEventListener("click",function calcular(e) {// on click, get the price of game + incl regional and national taxes, round value 
+calcularSubmit.addEventListener("click", function calcular(e) {
+    // on click, get the price of game + incl regional and national taxes, round value 
     e.preventDefault();
-    let finalPrice = precio + Math.round((precio * (tax + provImp) / 100) * 100) / 100;// round and show only 2 decimals
-    resultado.innerHTML = `<p class="title">Precio Final A Pagar Por ${nombreGame} Es: ${finalPrice}`;// insert <p> with the result
+    let finalPrice = precio + (precio * (taxValue + provImp)) / 100; // round and show only 2 decimals
+    resultado.innerHTML = `<p class="title">Precio Final A Pagar Por ${nombreGame} Es: ${finalPrice.toFixed(2)}`; // insert <p> with the result
+
+    // on click, store the value of the tax in localStorage
+    localStorage.setItem("impuestos", gameTax.value)
+
+    //on click, save index and value in localStorage
+    localStorage.setItem("provinciaIndex", prov.selectedIndex);
+    localStorage.setItem("provinciaValue", prov.value);
 })
 
-
-
-
-// calculate()
+// on load, if there is a previous tax value that is not 0, set it as default value in the input
+let localStrTax = localStorage.getItem("impuestos")
+if (localStrTax != "0") {
+    document.getElementById("taxValue").value = localStrTax;
+    taxValue = parseFloat(localStrTax);
+}
+// on load, if the previous prov is not null, set it as selected and set stored provImp value
+let localStrProvIndex = localStorage.getItem("provinciaIndex")
+let localStrProvValue = localStorage.getItem("provinciaValue")
+if (localStrProvIndex != null) {
+    document.getElementById("prov").selectedIndex = localStrProvIndex;
+    provImp = parseFloat(localStrProvValue);
+}
