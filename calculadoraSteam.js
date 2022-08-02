14a57ffecee10b1dc0c298097c16e620fdda0cd9
Leo-Cata <leonel.m.cata@gmail.com>
2022-08-01 21:26:03 -0300

    trabajo clase 12 y 13, operadores avanzados y librerias

--- calculadoraSteam.js
+++ calculadoraSteam.js
@@ -1,7 +1,12 @@
+// default tax value
+let taxValue = 75;
+
 let prov = document.getElementById("prov");
 let provImp;
-prov.addEventListener("change", function () { // get prov option value
+prov.addEventListener("change", function () { // get prov option value, on change, save index and value in localStorage
     provImp = parseFloat(prov.value)
+    localStorage.setItem("provinciaIndex", prov.selectedIndex);
+    localStorage.setItem("provinciaValue", prov.value);
 })
 
 let gameValue = document.getElementById("gameValue");
@@ -10,40 +15,28 @@ gameValue.addEventListener("change", function () { // get value input
     precio = parseFloat(gameValue.value)
 })
 
-let gameTax = document.getElementById("taxValue");
-let taxValue;
-gameTax.addEventListener("change", function () { // get tax value
-    taxValue = parseFloat(gameTax.value)
-})
-
 let gameName = document.getElementById("gameName")
-let nombreGame;
-gameName.addEventListener("change", function () { // get name 
-    nombreGame = gameName.value;
-})
+let nombreGame = "El Juego";
 
 let calcularSubmit = document.getElementById("btn__calcular");
 let resultado = document.getElementById("resultado");
+// on click, get the price of game + incl regional and national taxes, round value 
 calcularSubmit.addEventListener("click", function calcular(e) {
-    // on click, get the price of game + incl regional and national taxes, round value 
     e.preventDefault();
+    //
+    gameName.addEventListener("change", function () { // get name 
+        nombreGame == undefined ? nombreGame = "El Juego" : nombreGame = gameName.value;
+    })
+
+
     let finalPrice = precio + (precio * (taxValue + provImp)) / 100; // round and show only 2 decimals
-    resultado.innerHTML = `<p class="title">Precio Final A Pagar Por ${nombreGame} Es: ${finalPrice.toFixed(2)}`; // insert <p> with the result
+    if (finalPrice <= 0 || isNaN(finalPrice)) {
+        showError();
+    } else {
+        resultado.innerHTML = `<p class="title">Precio Final A Pagar Por ${nombreGame} Es: ${finalPrice.toFixed(2)}`;
+    }
+}) // insert <p> with the result
 
-    // on click, store the value of the tax in localStorage
-    localStorage.setItem("impuestos", gameTax.value)
-
-    //on click, save index and value in localStorage
-    localStorage.setItem("provinciaIndex", prov.selectedIndex);
-    localStorage.setItem("provinciaValue", prov.value);
-})
-
-// on load, if there is a previous tax value that is not 0, set it as default value in the input
-let localStrTax = localStorage.getItem("impuestos")
-if (localStrTax != "0") {
-    document.getElementById("taxValue").value = localStrTax;
-    taxValue = parseFloat(localStrTax);
-}
 // on load, if the previous prov is not null, set it as selected and set stored provImp value
 let localStrProvIndex = localStorage.getItem("provinciaIndex")
 let localStrProvValue = localStorage.getItem("provinciaValue")
@@ -51,3 +44,17 @@ if (localStrProvIndex != null) {
     document.getElementById("prov").selectedIndex = localStrProvIndex;
     provImp = parseFloat(localStrProvValue);
 }
+
+//if final price is nan show a pop up error
+function showError() {
+    Swal.fire({
+        icon: 'error',
+        title: 'Ocurrio un error',
+        text: 'Asegúrate De Haber Ingresado Valores Válidos Y Sin Separador De Mil.',
+        confirmButtonText: 'OK',
+        confirmButtonColor: '#292E3D',
+        customClass: 'sweetError',
+    })
+};
+
+// nombreGame = undefined ? nombreGame : nombreGame = "El Juego";
