06338f633cf765d0be817375e77650036d08835b
Leo-Cata <leonel.m.cata@gmail.com>
2022-08-22 17:12:53 -0300

    arrays implemented for final proyect

--- js/calculadoraSteam.js
+++ js/calculadoraSteam.js
@@ -5,7 +5,7 @@ const taxValue = 75;
 let prov = document.getElementById("prov");
 let provImp;
 prov.addEventListener("change", function () { // get prov option value, on change, save index and value in localStorage
-    provImp = parseFloat(prov.value)
+    provImp = parseFloat(prov.value);
     localStorage.setItem("provinciaIndex", prov.selectedIndex);
     localStorage.setItem("provinciaValue", prov.value);
 })
@@ -14,39 +14,43 @@ prov.addEventListener("change", function () { // get prov option value, on chang
 let gameValue = document.getElementById("gameValue");
 let precio;
 gameValue.addEventListener("change", function () { // get value input 
-    precio = parseFloat(gameValue.value)
+    precio = parseFloat(gameValue.value);
 })
 
 
-let gameName = document.getElementById("gameName")
-let nombreGame = "El Juego";
+let gameName = document.getElementById("gameName");
+let nombreGame = "El Juego"
+
+
+gameName.addEventListener("change", function () { // get name 
+    nombreGame == undefined ? nombreGame = "El Juego" : nombreGame = gameName.value;
+
+})
 
 
 let calcularSubmit = document.getElementById("btn__calcular");
 let resultado = document.getElementById("resultado");
-
-
+let calculatedPrice = [];
+let calculatedName = [];
 // on click, get the price of game + incl regional and national taxes, round value 
 calcularSubmit.addEventListener("click", function calcular(e) {
     e.preventDefault();
-    //
-    gameName.addEventListener("change", function () { // get name 
-        nombreGame == undefined ? nombreGame = "El Juego" : nombreGame = gameName.value;
-    })
-
-
     let finalPrice = precio + (precio * (taxValue + provImp)) / 100; // round and show only 2 decimals
     if (finalPrice <= 0 || isNaN(finalPrice)) {
         showError();
     } else {
         resultado.innerHTML = `<p class="title">Precio Final A Pagar Por ${nombreGame} Es: ${finalPrice.toFixed(2)}`;
+        calculatedPrice.push(finalPrice.toFixed(2));
+        calculatedName.push(nombreGame);
+        console.log(calculatedPrice);
+        console.log(calculatedName);
     }
 }) // insert <p> with the result
 
 
 // on load, if the previous prov is not null, set it as selected and set stored provImp value
-let localStrProvIndex = localStorage.getItem("provinciaIndex")
-let localStrProvValue = localStorage.getItem("provinciaValue")
+let localStrProvIndex = localStorage.getItem("provinciaIndex");
+let localStrProvValue = localStorage.getItem("provinciaValue");
 if (localStrProvIndex != null) {
     document.getElementById("prov").selectedIndex = localStrProvIndex;
     provImp = parseFloat(localStrProvValue);
@@ -64,3 +68,23 @@ function showError() {
         customClass: 'sweetError',
     })
 };
+
+
+const calculatedArray = document.getElementById("lista");
+const buttonArray = document.getElementById("history");
+let i = 0;
+
+
+//on click create a div for each element in arrays then clean arrays
+buttonArray.addEventListener("click", () => {
+    for (let element of calculatedName) {
+        let createdDiv = document.createElement("div");
+        createdDiv.innerHTML = `<div class="history__text container-fluid">El Precio Final A Pagar Por ${element} Es: ${calculatedPrice[i]} </div>`
+        calculatedArray.appendChild(createdDiv);
+        i++;
+    }
+    //reset arrays so it doesnt duplicate & reset i
+    calculatedName = [];
+    calculatedPrice = [];
+    i = 0;
+})
