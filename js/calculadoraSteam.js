1d9e3694cc0f462d05d1450dfcd5c98df9d1cf30
Leo-Cata <leonel.m.cata@gmail.com>
2022-08-23 16:03:12 -0300

    implementado error mensaje cuando el historial esta vacio + for of cambiado por for loop

--- js/calculadoraSteam.js
+++ js/calculadoraSteam.js
@@ -4,7 +4,10 @@ const taxValue = 75;
 
 let prov = document.getElementById("prov");
 let provImp;
-prov.addEventListener("change", function () { // get prov option value, on change, save index and value in localStorage
+
+
+// get prov option value on change, save it and value in localStorage
+prov.addEventListener("change", function () { 
     provImp = parseFloat(prov.value);
     localStorage.setItem("provinciaIndex", prov.selectedIndex);
     localStorage.setItem("provinciaValue", prov.value);
@@ -13,7 +16,7 @@ prov.addEventListener("change", function () { // get prov option value, on chang
 
 let gameValue = document.getElementById("gameValue");
 let precio;
-gameValue.addEventListener("change", function () { // get value input 
+gameValue.addEventListener("change", function () {
     precio = parseFloat(gameValue.value);
 })
 
@@ -22,9 +25,8 @@ let gameName = document.getElementById("gameName");
 let nombreGame = "El Juego"
 
 
-gameName.addEventListener("change", function () { // get name 
+gameName.addEventListener("change", function () {
     nombreGame == undefined ? nombreGame = "El Juego" : nombreGame = gameName.value;
-
 })
 
 
@@ -32,27 +34,27 @@ let calcularSubmit = document.getElementById("btn__calcular");
 let resultado = document.getElementById("resultado");
 let calculatedPrice = [];
 let calculatedName = [];
-// on click, get the price of game + incl regional and national taxes, round value 
+
+
+// on click, get the price of game + incl regional and national taxes, round value and insert <p> with the result
 calcularSubmit.addEventListener("click", function calcular(e) {
     e.preventDefault();
-    let finalPrice = precio + (precio * (taxValue + provImp)) / 100; // round and show only 2 decimals
+    let finalPrice = precio + (precio * (taxValue + provImp)) / 100;
     if (finalPrice <= 0 || isNaN(finalPrice)) {
         showError();
     } else {
         resultado.innerHTML = `<p class="title">Precio Final A Pagar Por ${nombreGame} Es: ${finalPrice.toFixed(2)}`;
         calculatedPrice.push(finalPrice.toFixed(2));
         calculatedName.push(nombreGame);
-        console.log(calculatedPrice);
-        console.log(calculatedName);
     }
-}) // insert <p> with the result
+})
 
 
 // on load, if the previous prov is not null, set it as selected and set stored provImp value
-let localStrProvIndex = localStorage.getItem("provinciaIndex");
+let localStrProvi = localStorage.getItem("provinciaIndex");
 let localStrProvValue = localStorage.getItem("provinciaValue");
-if (localStrProvIndex != null) {
-    document.getElementById("prov").selectedIndex = localStrProvIndex;
+if (localStrProvi != null) {
+    document.getElementById("prov").selectedIndex = localStrProvi;
     provImp = parseFloat(localStrProvValue);
 }
 
@@ -77,14 +79,26 @@ let i = 0;
 
 //on click create a div for each element in arrays then clean arrays
 buttonArray.addEventListener("click", () => {
-    for (let element of calculatedName) {
-        let createdDiv = document.createElement("div");
-        createdDiv.innerHTML = `<div class="history__text container-fluid">El Precio Final A Pagar Por ${element} Es: ${calculatedPrice[i]} </div>`
-        calculatedArray.appendChild(createdDiv);
-        i++;
+    if (!calculatedName.length || !calculatedPrice.length) {
+        emptyHistorial();
+    } else {
+        for (let i = 0; i < calculatedName.length; i++) {
+            let createdDiv = document.createElement("div");
+            createdDiv.innerHTML = `<div class="history__text container-fluid">El Precio Final A Pagar Por ${calculatedName[i]} Es: ${calculatedPrice[i]} </div>`
+            calculatedArray.appendChild(createdDiv);
+        }
+        calculatedName = [];
+        calculatedPrice = [];
     }
-    //reset arrays so it doesnt duplicate & reset i
-    calculatedName = [];
-    calculatedPrice = [];
-    i = 0;
 })
+
+
+function emptyHistorial() {
+    Swal.fire({
+        icon: 'error',
+        title: 'No Hay Elementos Para Mostrar!',
+        confirmButtonText: 'OK',
+        confirmButtonColor: '#292E3D',
+        customClass: 'sweetError',
+    })
+};
